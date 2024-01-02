3f429f796e1e1ac11573855a187846bdb260a539
2Proton2 <apurva22kalvade@gmail.com>
2024-01-03 04:59:27 +0530

    feat: protected the user-profile and logout api

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import {protectUser} from '../middlewares/auth.middleware.js';
 import asyncHandler from '../middlewares/asyncHandler.middleware.js'
 import * as userController from '../controllers/user.controller.js'
 const Router = express.Router();
 
 Router.post('/sign-in', asyncHandler(userController.signUser));
 Router.post('/login', asyncHandler(userController.loginUser));
-Router.post('/logout', asyncHandler(userController.logoutUser));
-Router.get('/', asyncHandler(userController.profileOfUser));
+Router.post('/logout', asyncHandler(protectUser), asyncHandler(userController.logoutUser));
+Router.get('/', asyncHandler(protectUser), asyncHandler(userController.profileOfUser));
 
 export default Router;
